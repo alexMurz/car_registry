5368203cfff10264e1801d92bce01b889f8c87df
alexmurz <alex_murz@icloud.com>
2020-10-29 12:04:58 +0300

    trait StatTracker now resides here Added usage counter (amount of times method called in total) Minor fixes

--- app/controllers/resources/Tracker.scala
+++ app/controllers/resources/Tracker.scala
@@ -6,21 +6,27 @@ import repo.{QResult, QSuccess}
 import scala.collection.mutable.ListBuffer
 import scala.concurrent.{ExecutionContext, Future}
 
+trait StatTracker {
+  def stats: Future[QResult]
+}
+
 // Single function timing track
 private class TimingTrack
 (
 // Track name
   val title: String,
-// Amount of data point we will preserve
-  val count: Int = 100,
+// Amount of data points we will preserve
+  val count: Int = 10,
 ) {
 
-  // List of last N timing
+  // List of last `count` timings
   private val times = ListBuffer.fill(count)(0L)
   // Precalculated total
   private var total = 0L
   // Current cursor position
   private var cursor = 0
+  // Usage counter. It is the same before buffer starts wrapping
+  private var usageCounter = 0
   // Checks as true if buffer was filled, and cursor wraps around at this point, hence we can consider all elements in buffer
   // And no only ones up to cursor
   private var wrapping = false
@@ -31,8 +37,8 @@ private class TimingTrack
     else times.take(cursor).toList
   }
 
-  // Add item
-  def add(v: Long): Unit = {
+  // Add timing entry
+  def add(v: Long): Unit = this.synchronized {
     val curr = cursor
     cursor = (cursor + 1) % count
     if (cursor == 0) wrapping = true
@@ -42,6 +48,7 @@ private class TimingTrack
 
   def measure[T](v: => Future[T])(implicit ec: ExecutionContext): Future[T] = {
     val t0 = System.nanoTime()
+    usageCounter += 1
     v.map { res =>
       add(System.nanoTime() - t0)
       res
@@ -51,31 +58,22 @@ private class TimingTrack
   def apply[T](v: => Future[T])(implicit ec: ExecutionContext): Future[T] = measure(v)
 
   def json: JsValue = {
-    val itemCount = if (wrapping) count else cursor
-    val average = if (itemCount == 0) 0.0
-    else (total / itemCount).doubleValue / 1e6
-
     val list = iter
-    val min = if (list.isEmpty) 0.0 else iter.min / 1e6
-    val max = if (list.isEmpty) 0.0 else iter.max / 1e6
+    val average = if (list.isEmpty) 0.0
+    else (total / list.size).doubleValue / 1e6
+
+    val min = if (list.isEmpty) 0.0 else list.min / 1e6
+    val max = if (list.isEmpty) 0.0 else list.max / 1e6
 
     JsObject(Seq(
       "name" -> JsString(title),
       "ty" -> JsString("timer"),
       "average_ms" -> JsNumber(average),
       "min_ms" -> JsNumber(min),
-      "max_ms" -> JsNumber(max)
+      "max_ms" -> JsNumber(max),
+      "usage" -> JsNumber(usageCounter)
     ))
   }
-}
-
-// Index insertion, removal track
-private class IndexTrack[T]
-(
-// Track name
-  val title: String
-) {
-
 }
 
 class TrackedResourceHandler(base: DataResourceHandler)(implicit ec: ExecutionContext) extends ResourceHandler with StatTracker {
@@ -83,7 +81,7 @@ class TrackedResourceHandler(base: DataResourceHandler)(implicit ec: ExecutionCo
   private val getAllTimer = new TimingTrack("getAll")
   override def getAll: Future[QResult] = getAllTimer(base.getAll)
 
-  private val lookupTimer = new TimingTrack("getAll")
+  private val lookupTimer = new TimingTrack("lookup")
   override def lookup(id: String): Future[QResult] = lookupTimer(base.lookup(id))
 
   private val insertTimer = new TimingTrack("insert")
