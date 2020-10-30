0f6bf3d05e84b5a112800effc6ec294ea48d1621
alexmurz <alex_murz@icloud.com>
2020-10-30 11:49:50 +0300

    Moved ListRepository to its own file, and added partial filtering support

---
+++ app/repo/ListRepository.scala
@@ -0,0 +1,76 @@
+package repo
+
+import javax.inject.Inject
+import play.api.Logger
+
+import scala.collection.mutable.ListBuffer
+import scala.concurrent.Future
+
+/// List Repository
+//@Singleton
+class ListRepository(list: ListBuffer[DataPoint])(implicit val ec: RepositoryContext)
+  extends DataRepository
+{
+  private val logger = Logger(getClass)
+
+  implicit class LocalFilter(filter: DataFilter) {
+    def test(point: DataPoint): Boolean = {
+      filter.col match {
+        case DataColumnId =>
+          // Transmute Bounds[String] to Bounds[Int]
+          logger.trace("Data ID filtering 1")
+          filter.bounds.transmute(_.toIntOption).exists { bounds =>
+            logger.trace(s"Try ID Filtersng with $bounds")
+            bounds.isInside(point.id.id)
+          }
+        case DataColumnNum => filter.bounds.isInside(point.num)
+        case DataColumnMark => filter.bounds.isInside(point.mark)
+        case DataColumnColor => filter.bounds.isInside(point.color)
+        case DataColumnDate => true // TODO
+      }
+    }
+  }
+
+  override def getAll(filters: Seq[DataFilter]): Future[Iterable[DataPoint]] = Future {
+    list.filter { dataPoint =>
+      logger.trace("AAA")
+      filters.forall { _.test(dataPoint) }
+    }
+  }
+
+  override def get(id: DataId): Future[Option[DataPoint]] = Future { list.find(_.id == id) }
+
+  override def insert(point: DataPoint): Future[DataId] = Future { ListRepository.this.synchronized {
+    val id = nextID
+    point.id = id
+    list += point
+    id
+  } }
+
+  override def remove(id: DataId): Future[Boolean] = Future { ListRepository.this.synchronized {
+    // ListBuffer does not have retain, or findIndexed?
+    list
+      .find(_.id == id)
+      .map(item => list.indexOf(item))
+      .exists(index =>
+        if (index >= 0) {
+          list.remove(index)
+          true
+        } else false
+      )
+  } }
+
+  /**
+   * @return next empty ID
+   */
+  private var ai = 0 // Auto Increment index
+  private def nextID: DataId = {
+    val i = ai
+    ai += 1
+    DataId(i)
+  }
+
+}
+
+class EmptyListRepository @Inject()(implicit ec: RepositoryContext) extends ListRepository(ListBuffer())(ec)
+
